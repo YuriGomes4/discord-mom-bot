fd2611d9e1db1a7bb4b54b83334b9ee6ab733e91
0x5b-5245444143544544-5d <vignesh.natarajan2021@vitstudent.ac.in>
2022-06-01 12:47:18 +0530

    Add sample bot code from py-cord repo

--- main.py
+++ main.py
@@ -1,27 +1,90 @@
+import os
+
 import discord
 import config
+from discord.commands import ApplicationContext, option
 
-bot = discord.Bot(debug_guilds=[719996466290098180])
+bot = discord.Bot(debug_guilds=config.guild_ids)
+bot.connections = {}
+discord.opus.load_opus(name=config.opus_path)
+
 
 @bot.command()
-async def start_record(ctx):
-    await ctx.author.voice.channel.connect() # Connect to the voice channel of the author
-    ctx.voice_client.start_recording(discord.sinks.MP3Sink(), finished_callback, ctx) # Start the recording
-    await ctx.respond("Recording...") 
+@option(
+    "encoding",
+    choices=[
+        "mp3",
+        "wav",
+        "pcm",
+        "ogg",
+        "mka",
+        "mkv",
+        "mp4",
+        "m4a",
+    ],
+)
+async def start(ctx: ApplicationContext, encoding: str):
+    """
+    Record your voice!
+    """
+    await ctx.defer()
 
-async def finished_callback(sink, ctx):
-    # Here you can access the recorded files:
-    recorded_users = [
-        f"<@{user_id}>"
-        for user_id, audio in sink.audio_data.items()
-    ]
+    voice = ctx.author.voice
+
+    if not voice:
+        return await ctx.respond("You're not in a vc right now")
+
+    vc = await voice.channel.connect()
+    bot.connections.update({ctx.guild.id: vc})
+
+    if encoding == "mp3":
+        sink = discord.sinks.MP3Sink()
+    elif encoding == "wav":
+        sink = discord.sinks.WaveSink()
+    elif encoding == "pcm":
+        sink = discord.sinks.PCMSink()
+    elif encoding == "ogg":
+        sink = discord.sinks.OGGSink()
+    elif encoding == "mka":
+        sink = discord.sinks.MKASink()
+    elif encoding == "mkv":
+        sink = discord.sinks.MKVSink()
+    elif encoding == "mp4":
+        sink = discord.sinks.MP4Sink()
+    elif encoding == "m4a":
+        sink = discord.sinks.M4ASink()
+    else:
+        return await ctx.respond("Invalid encoding.")
+
+    vc.start_recording(
+        sink,
+        finished_callback,
+        ctx.channel,
+    )
+
+    await ctx.respond("The recording has started!")
+
+
+async def finished_callback(sink, channel: discord.TextChannel, *args):
+    recorded_users = [f"<@{user_id}>" for user_id, audio in sink.audio_data.items()]
+    await sink.vc.disconnect()
     files = [discord.File(audio.file, f"{user_id}.{sink.encoding}") for user_id, audio in sink.audio_data.items()]
-    await ctx.channel.send(f"Finished! Recorded audio for {', '.join(recorded_users)}.", files=files) 
+    await channel.send(f"Finished! Recorded audio for {', '.join(recorded_users)}.", files=files)
+
 
 @bot.command()
-async def stop_recording(ctx):
-    ctx.voice_client.stop_recording() # Stop the recording, finished_callback will shortly after be called
-    await ctx.respond("Stopped!")
+async def stop(ctx):
+    """
+    Stop recording.
+    """
+    await ctx.defer()
+    if ctx.guild.id in bot.connections:
+        vc = bot.connections[ctx.guild.id]
+        vc.stop_recording()
+        del bot.connections[ctx.guild.id]
+        await ctx.delete()
+    else:
+        await ctx.respond("Not recording in this guild.")
 
 
 bot.run(config.bot_token)
