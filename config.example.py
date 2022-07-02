8446136e54c9d2f0e6ca7864a684fc7de1cc1059
0x5b-5245444143544544-5d <vignesh.natarajan2021@vitstudent.ac.in>
2022-07-02 20:35:53 +0530

    Add config variable

--- config.example.py
+++ config.example.py
@@ -1,3 +1,4 @@
 bot_token = ""
 guild_ids = [00000000000000000]
 opus_path = '/usr/lib/libopus.so' # replace with homebrew path for MacOS: /opt/homebrew/lib/libopus.dylib
+bucket_name = "" # replace with name of your firebase storage bucket
